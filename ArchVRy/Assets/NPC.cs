a6e11816d19956265be384c9bf51873e4b358816
Lucas Plabst <lplabst@colostate.edu>
2025-01-08 15:05:10 -0700

    adsd

---
+++ ArchVRy/Assets/NPC.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour
+{
+    Animator animator;
+    NavMeshAgent navigation;
+    GameObject target;
+    public int hp = 1;
+    public int dmg = 1;
+    bool isAttacking = false;
+    Gate gate;
+    void Start()
+    {
+        target = GameObject.Find("Gate");
+        gate = target.GetComponent<Gate>();
+        animator = GetComponent<Animator>();
+        navigation = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        navigation.destination = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < 2)
+        {
+            if (!isAttacking) Attack();
+        }
+    }
+
+    public void SetAnimation(string animation)
+    {
+        animator.Play(animation);
+    }
+    IEnumerator AttackStance()
+    {
+        yield return new WaitForSeconds(1);
+        if (gate.health > 0)
+        {
+            gate.health -= dmg;
+            StartCoroutine(AttackStance());
+        }
+        else
+        {
+            isAttacking = false;
+            SetAnimation("Wave");
+        }
+    }
+    void Attack()
+    {
+        isAttacking = true;
+        SetAnimation("Attack");
+        StartCoroutine(AttackStance());
+    }
+    void Die()
+    {
+        SetAnimation("Die");
+        Destroy(gameObject);
+    }
+}
